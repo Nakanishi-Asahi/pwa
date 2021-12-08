b5eca8fe767ab51cc0814637d51074189006ab80
Nakanishi-Asahi <48694062asahi@gmail.com>
2021-12-09 00:52:22 +0900

    2021/12/09

--- notification_app.js
+++ notification_app.js
@@ -6,51 +6,14 @@ window.addEventListener('load', () => {
             .catch(error => console.log('error', error));
     }
 });
-/*
+
 document
     .getElementById('confirm')
     .addEventListener('click', onClickConfirm);
 document.getElementById('send').addEventListener('click', onClickSend);
 document.getElementById('wait').addEventListener('click', onClickWait);
-*/
-new Vue({
-    el: "#app",
-    data(){
-        return {
 
-        }
-    },
-    methods: {
-        confirm(){
-            if (!('Notification' in window)) {
-                alert('このブラウザはプッシュ通知に対応してません。。。');
-                return;
-            }
-            Notification.requestPermission().then(permission => {
-                if (permission === 'granted') alert('通知の許可がもらえました');
-                if (permission === 'denied') alert('通知の許可がもらえませんでした');
-            });
-        },
-        send(){
-            if (!('Notification' in window)) {
-                alert('このブラウザはプッシュ通知に対応してません。。。');
-                return;
-            }
-            const permission = Notification.permission;
-            if (permission === 'granted') {
-                navigator.serviceWorker.ready.then(registration => {
-                    registration.active.postMessage("これはテスト通知です");
-                });
-            } else {
-                alert('通知の許可がもらえませんよ');
-            }
-        }
-    },
-    created(){
 
-    }
-});
-/*
 function onClickConfirm() {
     if (!('Notification' in window)) {
         alert('このブラウザはプッシュ通知に対応してません。。。');
